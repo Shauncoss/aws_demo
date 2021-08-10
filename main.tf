ad9ae6af249177caff85b294d1a22c9e879e962f
Shaun Coss <shauncoss1109@gmail.com>
2021-08-10 15:20:49 -0400

    fixed vars

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-data "aws_vpc" "vpc_id" {
-  id = var.vpc.id
-}
 resource "aws_key_pair" "ubuntu" {
   key_name   = "ubuntu-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDPfbmXx4xboyRkaUbtKmJSBILKc4kcWiqcR6+GLHgi9c721SH8iMPcvsyrDvj6BJoo8VDIFRAiIa2mZNKsi2a5ZL9u5ro35yTuyEv8nyZwjXAZgO8sXtuols8OMeR8I0466wIFmFDUukQzXPOYiHHTcpIVJ9aMMXqZRj5j8mu5on+yrivWyQ3VmnyqxzWZEYhiBHM335hj05yTR5BHJpSWBr0iXlTsstkoOC6+skhwm6klBA5vGQ46YhMaXaeBpZH1QEeZOQAORpguvoAiFQ2Mj2Bc9mMzZPygbznZBMY+Y9jyL1hHIMz3KmCqBQkV/NaEVl8+Rw5DLFbPRc7eqC+vW9gpPIa5tNbnsHOdNBnc7zcWrRcHoh3fbyTRmbyC8wNVe2OLMTz3mmSFOSfEvyfs+KLutKeZxP/ANREBlCgxpohu07JWFhPSDX0QeLFd5F75NZe8G10En/KMyom07nzqBb6Ao9IKO+kXMgp+M87i7PI4EZntUkSztJ50rBQA4jM= tatoe@tatoe-pc"
@@ -8,7 +5,7 @@ resource "aws_key_pair" "ubuntu" {
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
   description = "allow all"
-  vpc_id      = data.aws_vpc.vpc_id.id
+  vpc_id      = var.vpc
 
   ingress = [
     {
@@ -38,7 +35,7 @@ resource "aws_security_group" "allow_all" {
 }
 resource "aws_instance" "testvm" {
   ami           = var.ami_id
-  instance_type = var.instance_type
+  instance_type = "t2.micro"
   key_name      = aws_key_pair.ubuntu.key_name
   vpc_security_group_ids = [aws_security_group.allow_all.id]
   tags = {
@@ -46,5 +43,5 @@ resource "aws_instance" "testvm" {
   }
 }
 output "ec2_ip" {
-  value = aws_instance.testvm.public_ip
+  value = aws_instance.testvm.public_dns
 }
