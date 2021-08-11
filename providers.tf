a6a422e46832fab72892990efd60a743cc9d6a28
Shaun Coss <shauncoss1109@gmail.com>
2021-08-11 19:29:12 -0400

    asdf

--- providers.tf
+++ providers.tf
@@ -21,10 +21,11 @@ provider "vault" {
   address = "https://vault-cluster.vault.35108500-544b-45ec-b6a4-6230edfe5841.aws.hashicorp.cloud:8200"
   #add_address_to_env = true
   auth_login {
-    path = "auth/userpass/login/${var.username}"
+    #path = "auth/userpass/login/${var.username}"
+    path = "auth/userpass/login/aws_admin"
     namespace = "admin/aws/demo/"
     parameters = {
-      password = var.password
+      password = "awsdemopw123"
     }
   }
 }
