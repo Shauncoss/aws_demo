27ec0edc8a35f003753f3c5c2d2d2d9a1739f2b3
Shaun Coss <shauncoss1109@gmail.com>
2021-08-10 22:05:27 -0400

    fixed icmp

--- main.tf
+++ main.tf
@@ -21,8 +21,8 @@ resource "aws_security_group" "allow_ssh" {
     },
     {
       description      = "Allow ICMP"
-      from_port        = 0
-      to_port          = 0
+      from_port        = "-1"
+      to_port          = "-1"
       protocol         = "icmp"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = null
